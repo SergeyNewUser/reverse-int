c31d61a9288d5e3eeaa027225d87b8be29cf026b
SergeyNewUser <DevelopNewJS@gmail.com>
2021-07-18 14:20:46 +0300

    Task reverse-int comleted

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+   if (n < 0) n = n * -1;
+   n = n.toString();
+   return c = n.split('').reverse().join('');
+
 }
